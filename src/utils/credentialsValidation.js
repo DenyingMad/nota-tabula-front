916fbbe79727ffacb27f9b5589a9693639bb18ac
Danny <mezenov.201521@gmail.com>
2021-03-21 23:49:08 +0300

    Добавлена валидация пароля по словарю

--- src/utils/credentialsValidation.js
+++ src/utils/credentialsValidation.js
@@ -1,4 +1,27 @@
-const weakPasswords = ['qwerty_123', 'password'];
+const weakPasswords = 'qwerty_123456789passwordadmin!root01234567891234admin123asdf123go13131313131314151617181922020002121122222222323232324252627282933031323333333435363738394404141284243432144444445464748495505151505253545555555657585966061626364656543216666666666666768696969696969770717273747576777777777777777777778798808182838485868675309878889990919293949596979898765499ADMINAdminAdministatorDemoKKKKKKKManagerPASSWORDQwertyTESTaaaaaaaaaaabc123abd234abgrtyuaccessaccess14actionadmadminadministratoradmnalbertalexalexisamandaamateurandreaandrewangelangelaangelsanimalanthonyapolloappleapplesarsenalarthurasdfasdf123asdfgasdfghashleyassholeaugustaustinbabybackupbackupexecbadboybaileybananabarneybaseballbatmanbeachbearbeaverbeavisbeerbigcockbigdaddybigdickbigdogbigtitsbillbillybirdiebitchbitchesbitemeblackblankblazerblondeblondesblowjobblowmebluebond007bonnieboobooboobsboogerboomerbootybostonbrandonbrandybravesbrazilbrianbroncobroncosbubbabuddybulldogbusterbutterbuttheadcalvincamarocameroncanadacaptaincarloscartercasperchangemecharlescharliecheesechelseachesterchevychicagochickenchrisciscoclustadmclustercocacolacockcoffeecollegecompaqcompaqcomputercookiecookie123coolcoopercorrectcorvettecowboycowboyscreamcrystalcummingcumshotcuntdakotadallasdanieldanielledavedaviddebbiedefaultdefaultdelldemodennisdiablodiamonddickdirtydmzdoctordoggiedolphindolphinsdominodonalddragondreamsdrivereagleeagle1eagleseatdirtedwardeinsteinenjoyenterericeroticexchadmexchangeextremefalconfenderferrarifictionfirefirebirdfishfishingfloridaflowerflyersfoobarfootballfordforeverfrankfredfreddyfreedomftpfuckfuckedfuckerfuckingfuckmefuckyoufuckyou1gandalfgatewaygatewaygatorsgeminigeorgegiantsgingergirlgirlsgodgoldengolfgolfergordongreatgreengregoryguestguestguitargunnerguruhammerhannahhappyhardcoreharleyheatherhellohelpmehentaihockeyhootershorneyhornyhotdoghousehunterhuntingicemaniloveyouiloveyou1internetiwantujackjackiejacksonjaguarjakejamesjapanjasminejasonjasperjenniferjeremyjessicajohnjohnnyjohnsonjordanjosephjoshuajuicejuniorjustinkellykevinkillerkingkittyknightladieslakerslaurenleatherlegendletmeinlittleloginlondonlotusloveloverloversluckylustmaddogmadisonmaggiemagicmagnummailmanagermarinemarkmarlboromartinmarvinmastermatrixmattmatthewmaverickmaxwellmelissamembermercedesmerlinmichaelmichellemickeymidnightmikemillerminemiss4youmistressmoneymoneymonicamonitormonkeymonstermorganmothermountainmoviemuffinmurphymusicmustangnakednascarnathannaughtyncc1701neonewpassnewusernewyorknicholasnicolenipplenipplesnononenopasswordnotesofficeoliveroracleorangeou812paSSworDpackerspantherpantiesparisparkerpasspasspasscodepasswdpasswordpasswordpassword!password1password1patrickpaulpeachespeanutpenispepperpeterphantomphoenixplayerpleasepoiqwepookiepornpornoporschepowerprinceprincessprintprivatepurplepussiespussyqazwsxqweasdqwertqwertyqwertyqwertyuirabbitrachelracingraidersrainbowrangerrangersreadrebeccaredskinsredsoxredwingsreplicaterichardrobertrockrocketrootrosebudrunnerrush2112russiasasamanthasammysamsonsandrasaturnscoobyscooterscorpioscorpionscottseagatesecretsecretsecuritysetupsexsexsexsexyshadowshannonshavedshitsierrasilverskippyslayerslutsmithsmokeysnoopysoccersophiespacespankysparkyspidersqlsqlexecsquirtsrinivasstarstarsstartrekstarwarssteelersstevestevenstickystupidsuccesssuckitsummersunshinesupersupermansuperusersurferswimmingsydneysyssysadmsysadminsystemtaylortechteenstemptemptemp!temp123tennisteresatesttesttest!test123testertestingthemanthomasthunderthx1138tiffanytigertigerstiggertimetitstivolitmptomcattoortoptopguntoyotatravistroubletrustno1tsttuckerturtleuniteduservaginaveritasvictorvictoriavideovikingvipervirusvoodoovoyagerwalterwarriorwebwelcomewhateverwhitewilliamwilliewilsonwinnerwinstonwinterwizardwolfwomenwordpasswritewwwxavierxxxxxxxxxxxxxxxxxxxxxxxxxxyamahayankeeyankeesyellowyoungzxcvbnzxcvbnmzzzzzz';
+
+const z_function = (password) => {
+    let s = password + '`' + weakPasswords;
+    const n = s.length;
+    const z = Array(n).fill(0);
+    for (let i = 0, l = 0, r = 0; i < n; i++) {
+        if (i <= r)
+            z[i] = Math.min(r - i + 1, z[i - l]);
+        while(i + z[i] < n && s[z[i]] === s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+    return z;
+}
+
+const checkPasswordWeakness = (password) => {
+    const z = z_function(password);
+    z[0] = 0;
+    return Math.max(...z);
+}
 
 export const passwordValidation = (password) => {
     return new Promise((resolve, reject) => {
@@ -9,31 +32,36 @@ export const passwordValidation = (password) => {
             number = /[0-9]/,
             space = /\s/,
             special = /[!#$%&'()*+,./:;<=>?@^_{|}~-]/;
+        let score = 5;
         // tests
         if (space.test(password)) {
-            dynamicErrorMsg =  "Пароль не должен быть пустым или содержать пробелы"
-            reject(dynamicErrorMsg)
+            score--;
+            dynamicErrorMsg =  "Пароль не должен быть пустым или содержать пробелы";
+            reject(dynamicErrorMsg);
         }
         if (!upper.test(password)) {
-            dynamicErrorMsg = "Password must contain at least one uppercase letter"
-            reject(dynamicErrorMsg)
+            score--;
+            dynamicErrorMsg = "Password must contain at least one uppercase letter";
+            reject(dynamicErrorMsg);
         }
         if (!lower.test(password)) {
-            dynamicErrorMsg = "Password must contain at least one lowercase letter"
-            reject(dynamicErrorMsg)
+            score--;
+            dynamicErrorMsg = "Password must contain at least one lowercase letter";
+            reject(dynamicErrorMsg);
         }
         if (!number.test(password)) {
-            dynamicErrorMsg = "Password must contain at least one number"
-            reject(dynamicErrorMsg)
+            score--;
+            dynamicErrorMsg = "Password must contain at least one number";
+            reject(dynamicErrorMsg);
         }
         if (!special.test(password)) {
-            dynamicErrorMsg = "Password must contain at least one special character"
-            reject(dynamicErrorMsg)
+            score--;
+            dynamicErrorMsg = "Password must contain at least one special character";
+            reject(dynamicErrorMsg);
         }
-        // check if password matches any of simple passwords
-        if (password && weakPasswords.indexOf(password.toLowerCase()) > -1) {
-            dynamicErrorMsg = "Password is too simple"
-            reject(dynamicErrorMsg)
+        if (score - checkPasswordWeakness(password) <= 0) {
+            dynamicErrorMsg = "Password is too weak";
+            reject(dynamicErrorMsg);
         }
         resolve("Password is good")
     })
