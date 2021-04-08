ddce1b5134166f6f937a2a62314c21e674751482
Danny <mezenov.201521@gmail.com>
2021-04-08 14:34:09 +0300

    Убраны вкладки авторизации из сайдбара

--- src/app/AppView.jsx
+++ src/app/AppView.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import {Route, Switch} from "react-router-dom";
-import {Register} from "../pages/register/Register";
-import {Login} from "../pages/login/Login";
+import {Switch} from "react-router-dom";
 import Stub from "../pages/stub/Stub";
 import AuthRoute from "../security/AuthRoute";
 import {Dashboard} from "../pages/dashboard/Dashboard";
@@ -10,8 +8,6 @@ import {Dashboard} from "../pages/dashboard/Dashboard";
 const AppView = () => {
     return (
         <Switch>
-            <Route path="/login" component={Login} exact/>
-            <Route path="/join" component={Register} exact/>
             <AuthRoute path="/dashboard" component={Dashboard} exact/>
             <AuthRoute path="/reports" component={Stub} exact/>
         </Switch>
