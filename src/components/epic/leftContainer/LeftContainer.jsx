13cfeeb1b4f876594bed331636befa7d105a545c
Yesquero <69271414+Yesquero@users.noreply.github.com>
2021-04-23 23:51:00 +0300

    Функционал изменения имени эпика интегрирован с сервером.

--- src/components/epic/leftContainer/LeftContainer.jsx
+++ src/components/epic/leftContainer/LeftContainer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import {LeftContainerView} from "./LeftContainerView";
+import {renameEpic} from "../../../api/EpicApi";
 
 export const LeftContainer = (props) => {
-    const handlerRenameEpic = () => {
+    const handlerRenameEpic = (value, inputProps) => {
+        renameEpic(inputProps.epicid, value)
+            .then(r => r)
+            .catch(error => console.log(error))
     };
 
     return (
