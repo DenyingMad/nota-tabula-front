96d603bcb4c66620a63a498d908db6a62022083d
Yesquero <69271414+Yesquero@users.noreply.github.com>
2021-05-25 21:47:21 +0300

    Добавлены поля изменения имени, пароля. Изменение пароля не работает.

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,22 @@
-import React from "react";
-import {useProfileStyles} from "./ProfileStyles";
-import {Card, TextField, Typography} from "@material-ui/core";
-import clsx from "clsx";
+import React, {useContext, useEffect, useState} from "react";
 import * as Yup from "yup";
 import {passwordValidation} from "../../utils/credentialsValidation";
+import {AppContext} from "../../AppContext";
+import {changeUsername, changeUserPassword, getUserDetails, getUserDetailsByLogin} from "../../api/UserApi";
+import {useFormik} from "formik";
+import {ProfileView} from "./ProfileView";
+
+const handleChangePassword = (setError) => (values) => {
+    // dont call this yet
+
+    return changeUserPassword(values)
+        .then(r => {
+        })
+        .catch(error => {
+            setError("Sample Error.");
+            console.log(error);
+        });
+};
 
 const validationSchema = Yup.object({
     password: Yup
@@ -20,28 +33,72 @@ const validationSchema = Yup.object({
                     return this.createError({message: errorMessage});
                 });
         })
-})
+});
 
 export const Profile = () => {
-    const classes = useProfileStyles();
+    const [context, setContext] = useContext(AppContext);
+    const [userDetails, setUserDetails] = useState({});
+    const [newUsername, setNewUsername] = useState("");
+    const [newPassword, setNewPassword] = useState("If you see this, there's an error");
+    const [PasswordError, setPasswordError] = useState();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onChangePassword = handleChangePassword(setPasswordError);
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
+    const handleLoginChange = (event) => {
+        setNewUsername(event.target.value)
+    };
+    const handleSubmitUserName = (newUserName) => {
+        // call API
+        changeUsername(newUsername)
+            .then(r => {})
+            .catch(err => console.log(err))
+    };
+
+    useEffect(() => {
+        let cleanupFunction = false;
+        getUserDetailsByLogin(context.currentUser)
+            .then(r => {
+                if (!cleanupFunction) {
+                    setUserDetails(r);
+                    setNewPassword(r.password);
+                }
+            })
+            .catch(error => console.log(error));
+        getUserDetails()
+            .then(r => {
+                if(r.userName) setNewUsername(r.userName);
+                else setNewUsername("Default userName");
+            })
+            .catch(err => console.log(err))
+        return () => cleanupFunction = true;
+    }, []);
+
+    const formik = useFormik({
+        initialValues: {
+            password: newPassword,
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            return onChangePassword(values);
+        }
+    });
 
     return (
-        <Card
-            className={clsx(classes.flexColumn, classes.fullWidth, classes.profileCard)}
-        >
-            <Typography variant="h4" align="left">
-                {"User Name"}
-            </Typography>
-            <Typography variant="body1" align="left">
-                Your current user name.
-            </Typography>
-            <TextField
-                id="change-username"
-                label="Current Username"
-                defaultValue="sample_username"
-                helperText="Change your user name here."
-                className={clsx(classes.textField)}
-            />
-        </Card>
+        <ProfileView
+            values={formik.values}
+            userDetails={userDetails}
+            newUsername={newUsername}
+            handleLoginChange={handleLoginChange}
+            handleSubmitUserName={handleSubmitUserName}
+            handlePasswordChange={formik.handleChange}
+            handleSubmit={formik.handleSubmit}
+            errors={formik.errors}
+            message={PasswordError}
+            touched={formik.touched}
+            newPassword={newPassword}
+            passwordVisibility={showPassword}
+            handleClickShowPassword={handleClickShowPassword}
+        />
     )
-}
+};
