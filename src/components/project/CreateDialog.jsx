2ff359a18fe4ead737a7f854589c2646e50169b9
Yesquero <69271414+Yesquero@users.noreply.github.com>
2021-06-02 13:56:33 +0300

    Форматирование кода.

--- src/components/project/CreateDialog.jsx
+++ src/components/project/CreateDialog.jsx
@@ -1,4 +1,4 @@
-import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from "@material-ui/core";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import {CreateProjectForm} from "./CreateProjectForm";
 import {useFormik} from "formik";
